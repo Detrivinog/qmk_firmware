42fa35960fafc3df44f5bce24bcd4e8914ba712e
Detrivinog <detrivinog@gmail.com>
2025-12-28 22:22:52 -0500

    complete adjustments

--- keyboards/sofle/keymaps/david/keymap.c
+++ keyboards/sofle/keymaps/david/keymap.c
@@ -13,11 +13,11 @@ enum sofle_layers {
 };
 
 enum custom_keycodes {
-    KC_PRVWD = QK_USER,
+    KC_PRVWD = SAFE_RANGE,
     KC_NXTWD,
     KC_LSTRT,
     KC_LEND,
-    INV_QUES = SAFE_RANGE,
+    INV_QUES,
     INV_EXCLM,
     ENE,
 };
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     HYPR_T(KC_ESC),   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
     LT(_NUM, KC_TAB), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
     KC_LSFT,          KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MPLY,      KC_MUTE,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                    KC_LGUI,KC_LALT,KC_LCTL, TL_LOWR, KC_SPC,                 KC_ENT, LT(_RAISE, KC_SPC), KC_RCTL, KC_RALT, KC_RGUI
+                    KC_LGUI,KC_LALT,KC_LCTL, MO(_LOWER), KC_SPC,                 KC_ENT, MO(_RAISE), KC_RCTL, KC_RALT, KC_RGUI
 ),
 /*
 * NUM
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, QK_LLCK, _______,                        KC_MINS,    KC_7,    KC_8,    KC_9,  KC_DOT, KC_BSPC,
     LT(_NUM, KC_TAB), _______, _______, _______, KC_LSFT, _______,               KC_PLUS,    KC_4,    KC_5,    KC_6,    KC_0, KC_CIRC,
     KC_LSFT, _______, _______, _______, _______, _______, _______,      _______, KC_ASTR,    KC_1,    KC_2,    KC_3, KC_SLSH, KC_COMM,
-                KC_LGUI,KC_LALT,KC_LCTL,TL_LOWR, KC_ENT,                    KC_SPC,  TL_UPPR, KC_RCTL, KC_RALT, KC_RGUI
+                KC_LGUI,KC_LALT,KC_LCTL,MO(_LOWER), KC_ENT,                    KC_SPC,  MO(_RAISE), KC_RCTL, KC_RALT, KC_RGUI
 ),
 /* LOWER
  * ,----------------------------------------.                      ,-----------------------------------------.
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,  KC_INS,  KC_PSCR,   KC_APP,  QK_LLCK, KC_CAPS,                        QK_LLCK, KC_PRVWD,   KC_UP, KC_NXTWD,C(KC_BSPC), KC_BSPC,
   _______, KC_LALT,  KC_LCTL,  KC_LSFT,  KC_MEH, KC_HYPR,                        XXXXXXX,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, QK_LLCK,
   _______, KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, XXXXXXX,  _______,       _______,  ENE, KC_LSTRT, XXXXXXX, KC_LEND,   XXXXXXX, _______,
-                    KC_LGUI,KC_LALT,KC_LCTL,TL_LOWR, KC_ENT,                 KC_SPC,  TL_UPPR, KC_RCTL, KC_RALT, KC_RGUI
+                    KC_LGUI,KC_LALT,KC_LCTL,MO(_LOWER), KC_ENT,                 KC_SPC,  MO(_RAISE), KC_RCTL, KC_RALT, KC_RGUI
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -163,7 +163,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   QK_BOOT , XXXXXXX, XXXXXXX, KC_COLEMAK, QK_LLCK,XXXXXXX,                     XXXXXXX, UG_VALU, UG_SATU, UG_HUEU, KC_BRIU, XXXXXXX,
   XXXXXXX , XXXXXXX, CG_TOGG, XXXXXXX,    KC_LSFT,  XXXXXXX,                     XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, KC_BRID, XXXXXXX,
   XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,  XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
-                   KC_LGUI,KC_LALT,KC_LCTL,TL_LOWR, KC_ENT,                    KC_SPC,  TL_UPPR, KC_RCTL, KC_RALT, KC_RGUI
+                   KC_LGUI,KC_LALT,KC_LCTL,MO(_LOWER), KC_ENT,                    KC_SPC,  MO(_RAISE), KC_RCTL, KC_RALT, KC_RGUI
 ),
 /*
 * COLEMAK
@@ -185,7 +185,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     HYPR_T(KC_ESC),   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC,
     LT(_NUM, KC_TAB),  KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                     KC_H,    KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT,
     KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MPLY,             KC_MUTE, KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                KC_LGUI, KC_LALT, KC_LCTL, TL_LOWR, KC_SPC,              KC_ENT, LT(_RAISE, KC_SPC), KC_RCTL, KC_RALT, KC_RGUI
+                KC_LGUI, KC_LALT, KC_LCTL, MO(_LOWER), KC_SPC,              KC_ENT, MO(_RAISE), KC_RCTL, KC_RALT, KC_RGUI
 )
 };
 
@@ -211,7 +211,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
         case KC_NXTWD:
-             if (record->event.pressed) {
+            if (record->event.pressed) {
                 if (keymap_config.swap_lctl_lgui) {
                     register_mods(mod_config(MOD_LALT));
                     register_code(KC_RIGHT);
@@ -279,8 +279,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         case ENE:
             if (record->event.pressed) {
-                tap_code(KC_TILD); // ~ (dead key)
-                tap_code(KC_N);    // n
+                tap_code16(RALT(KC_N));
             }
             return false;
     }
@@ -289,6 +288,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 
 layer_state_t layer_state_set_user(layer_state_t state) {
+
+    state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
     // Obtener saturación y valor actuales
     uint8_t current_sat = rgblight_get_sat();
     uint8_t current_val = rgblight_get_val();
